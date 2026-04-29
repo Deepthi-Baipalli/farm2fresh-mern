736398e88b5f8017aba4cd228d5874fa3773c8b2
Deepthi Baipalli <deepthichinnni@gmail.com>
2026-04-29 15:45:50 +0530

    Fix syntax error in user login query

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email);
+  const user = await User.findOne({ email });
   if (!user) return res.status(400).json({ message: "User not found" });
 
   const valid = await bcrypt.compare(password, user.password);
